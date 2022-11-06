5323f05b74a81d6f3184b667de281ebdaa3148a8
Nabil Kannane <88660127+NabilKannane@users.noreply.github.com>
2022-11-06 14:19:53 +0100

    first step done

--- Hanout/app.js
+++ Hanout/app.js
@@ -6,12 +6,13 @@ const prods = require('./produit');
 
 const home = fs.readFileSync(__dirname+"/template/home.html","utf-8",);
 const contact = fs.readFileSync(__dirname+"/template/contact.html","utf-8");
+const addproduct = fs.readFileSync(__dirname+"/template/addproduct.html","utf-8");
 
 const server = http.createServer((request,response)=>{
     var path = url.parse(request.url).pathname;    
 
     if(path == "/"){
-        fs.readFile(__dirname+"/template/index.html",function(err,data){
+        fs.readFile(__dirname+"/template/home.html",function(err,data){
             if(err){
                 response.writeHead(404);
                 response.write("file not found !");
@@ -25,7 +26,7 @@ const server = http.createServer((request,response)=>{
                                     <h4>Ram : ${prods[i].ram}</h4>
                                     <h4>HDD : ${prods[i].hdd} ${prods[i].isSSD}</h4>
                                     <h4>CPU : ${prods[i].processeur}</h4>
-                                    <h4>description : ${prods[i].description}</h4>
+                                    <p>description : ${prods[i].description}</p>
                                     </div>`;
           }
                     data = home.replace("{% produits %}",produits);
@@ -35,28 +36,65 @@ const server = http.createServer((request,response)=>{
         });
 
     }
+
+      // ================================== Contact =====================================
+
     if(path == "/contact"){
-        fs.readFile(__dirname+"/template/index.html", function(err,data){
+        fs.readFile(__dirname+"/template/contact.html", function(err,data){
             if(err){
                 response.writeHead(404);
                 response.write("File not found !");
             }else{
+                data=contact.replace("{% contact %}",
+                `<form action="">
+                <h1>Contact Us</h1>
+                <label for="email">Email :</label>
+                <input type="email" name="email" placeholder="exemple@mail.com" width="150"><br>
+                <label for="content">Content :</label><br>
+                <textarea name="content" id="" cols="100" rows="10"></textarea></form>`)
+                response.writeHead(200);
                 response.write(data);
             }
             response.end();
         })
     }
+
+  // ================================== Addproduct =====================================
+
     if(path == "/addproduct"){
-        fs.readFile(__dirname+"/template/index.html", function(err,data){
+        fs.readFile(__dirname+"/template/addproduct.html", function(err,data){
             if(err){
                 response.writeHead(404);
                 response.write("File not found !");
             }else{
+                data = addproduct.replace("{% form %}",`<form action="">
+                <label for="marque">Marque :</label>
+                <input class="input" type="text" placeholder="Marque" name="marque"><br>
+                <label for="mdoel">Model :</label>
+                <input class="input" type="text" placeholder="Model" name="model"><br>
+                <label for="ram">Ram :</label>
+                <input class="input" type="number" placeholder="Ram" name="ram"><br>
+                <label for="hdd">HDD :</label>
+                <input class="input" type="number" placeholder="HDD" name="hdd"><br>
+                <label for="ram">is SSD :</label><br>
+                <input type="radio" >Yes<br>
+                <input type="radio" >Non<br>
+                <label for="cpu">CPU :</label>
+                <input class="input" type="text" placeholder="CPU" name="cpu"><br>
+                <label for="image">Image :</label>
+                <input class="input" type="text" placeholder="Link Image" name="image"><br>
+                <label for="description">Description :</label>
+                <input class="input" type="text" placeholder="description" name="description"><br>
+                <button>Ajouter</button>
+            </form>`)
+                response.writeHead(200);
                 response.write(data);
             }
             response.end();
         })
     }
+
+    // ================================== STYLE.CSS =====================================
 
     if(request.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
         fs.readFile(__dirname + '/template/style/style.css', function (err, data) {
--- Hanout/produit.js
+++ Hanout/produit.js
@@ -8,7 +8,7 @@ const prods=[
         isSSD : true,
         processeur :'i7',
         image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqsX6HkXMkqsqXO8KYqOJeAuocw66DKvhEWR8_mlcisnrsf9giSulPdiwjihRdpUznYtI&usqp=CAU',
-        description :'loremh jkhj hvj jh jh kjhfhgfghj'
+        description :'Lorem ipsum dolor sit amet...'
     }
     ,
     {
@@ -20,7 +20,7 @@ const prods=[
         isSSD : true,
         processeur :'i7',
         image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqPCvS4qlu1tjsODLKSYiRPnc64lX_KKp_ERyTKFtvlkddiGEuQR-TUpS6vaA2eNeljVw&usqp=CAU',
-        description :'loremh jkhj hvj jh jh kjhfhgfghj'
+        description :'Lorem ipsum dolor sit amet...'
     }
     ,
     {
@@ -32,7 +32,54 @@ const prods=[
         isSSD : true,
         processeur :'i7',
         image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlWGkCBvawOL_TdMR_YqWZ52sGk3fuv00JEQ&usqp=CAU',
-        description :'loremh jkhj hvj jh jh kjhfhgfghj'
+        description :'Lorem ipsum dolor sit amet...'
+    }
+    ,
+    {
+        id:4,
+        marque :'Lenovo',
+        modele : 'elitebook',
+        ram : '8 GB',
+        hdd :'500 GB',
+        isSSD : true,
+        processeur :'i7',
+        image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlWGkCBvawOL_TdMR_YqWZ52sGk3fuv00JEQ&usqp=CAU',
+        description :'Lorem ipsum dolor sit amet...'
+    }
+    ,
+    {
+        id:5,
+        marque :'Lenovo',
+        modele : 'elitebook',
+        ram : '8 GB',
+        hdd :'500 GB',
+        isSSD : true,
+        processeur :'i7',
+        image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlWGkCBvawOL_TdMR_YqWZ52sGk3fuv00JEQ&usqp=CAU',
+        description :'Lorem ipsum dolor sit amet...'
+    }
+    ,
+    {
+        id:6,
+        marque :'Dell',
+        modele : 'XPS',
+        ram : '16 GB',
+        hdd :'500 GB',
+        isSSD : true,
+        processeur :'i7',
+        image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqPCvS4qlu1tjsODLKSYiRPnc64lX_KKp_ERyTKFtvlkddiGEuQR-TUpS6vaA2eNeljVw&usqp=CAU',
+        description :'Lorem ipsum dolor sit amet...'
+    },
+    {
+        id:7,
+        marque :'Hp',
+        modele : 'elitebook',
+        ram : '8 GB',
+        hdd :'500 GB',
+        isSSD : true,
+        processeur :'i7',
+        image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqsX6HkXMkqsqXO8KYqOJeAuocw66DKvhEWR8_mlcisnrsf9giSulPdiwjihRdpUznYtI&usqp=CAU',
+        description :'Lorem ipsum dolor sit amet...'
     }
 ];
 
